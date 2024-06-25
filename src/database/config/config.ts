a2e098355d299ea5e82a05ce1ba13839772066c9
Renato Filho <renatosousafilho@gmail.com>
2024-06-25 12:12:31 -0300

    aplica environments na config do sequelize

--- src/database/config/config.ts
+++ src/database/config/config.ts
@@ -2,19 +2,25 @@ import pg from 'pg';
 
 // definir uma forma para ler de acordo com o ambiente
 export = {
-  dialect: 'postgres',
-  host: process.env.POSTGRES_HOST || 'localhost',
-  port: parseInt(process.env.POSTGRES_PORT) || 5432,
-  username: process.env.POSTGRES_USER || 'postgres',
-  password: process.env.POSTGRES_PASSWORD || 'postgres',
-  database: process.env.POSTGRES_DATABASE || 'trybecar',
-  dialectModule: pg,
-  // A migration rodou quando deixei isso comentado para rodar local
-  // Para rodar na nuvem preciso disso
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
+  development: {
+    dialect: 'sqlite',
+    storage: ':memory:',
+  },
+  production: {
+    dialect: 'postgres',
+    host: process.env.POSTGRES_HOST || 'localhost',
+    port: parseInt(process.env.POSTGRES_PORT) || 5432,
+    username: process.env.POSTGRES_USER || 'postgres',
+    password: process.env.POSTGRES_PASSWORD || 'postgres',
+    database: process.env.POSTGRES_DATABASE || 'trybecar',
+    dialectModule: pg,
+    // A migration rodou quando deixei isso comentado para rodar local
+    // Para rodar na nuvem preciso disso
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
     },
   },
 };
