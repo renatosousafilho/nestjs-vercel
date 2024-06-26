6b5090ce35ecd4c17f081b8dd1b0bfd97af52f32
Renato Filho <renatosousafilho@gmail.com>
2024-06-26 10:53:57 -0300

    refactor: ajusta conexão para o banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,28 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PassengersModule } from './passengers/passengers.module';
 import { SequelizeModule } from '@nestjs/sequelize';
-import * as pg from 'pg';
-import Passenger from './passengers/passenger.model';
+import config = require('./database/config/config');
+
+console.log(config[process.env.NODE_ENV || 'development']);
 
 @Module({
   imports: [
     PassengersModule,
     SequelizeModule.forRoot({
-      dialect: 'postgres',
-      host: process.env.POSTGRES_HOST || 'localhost',
-      port: parseInt(process.env.POSTGRES_PORT) || 5432,
-      username: process.env.POSTGRES_USER || 'postgres',
-      password: process.env.POSTGRES_PASSWORD || 'postgres',
-      database: process.env.POSTGRES_DATABASE || 'trybecar',
-      synchronize: true,
-      dialectModule: pg,
-      dialectOptions: {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false,
-        },
-      },
-      models: [Passenger],
+      ...config[process.env.NODE_ENV || 'development'],
+      autoLoadModels: true,
     }),
   ],
   controllers: [AppController],
--- src/database/config/config.ts
+++ src/database/config/config.ts
@@ -3,6 +3,14 @@ import pg from 'pg';
 // definir uma forma para ler de acordo com o ambiente
 export = {
   development: {
+    dialect: 'postgres',
+    host: process.env.POSTGRES_HOST || 'localhost',
+    port: parseInt(process.env.POSTGRES_PORT) || 5432,
+    username: process.env.POSTGRES_USER || 'postgres',
+    password: process.env.POSTGRES_PASSWORD || 'postgres',
+    database: process.env.POSTGRES_DATABASE || 'trybecar',
+  },
+  test: {
     dialect: 'sqlite',
     storage: ':memory:',
   },
